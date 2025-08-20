0943c7026590f8a6672f70bcf301156326cf7a6a
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2025-08-20 08:30:05 -0700

    added some content to weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 export default function Weather() {
-    return (
-        <h1>Hello from weather</h1>
-    )
+  return (
+    <div>
+      <h3>React Weather app</h3>
+      <form>
+        <input type="search" placeholder="Enter a city..." />
+        <button type="submit">Search</button>
+      </form>
+      <h1>San Diego</h1>
+      <p>Wednesday 08:23am, sunny</p>
+      <p>Humidity: 60%, Wind: 5mph</p>
+    </div>
+  );
 }
