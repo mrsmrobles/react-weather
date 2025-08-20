60007164146e55fac0e551ce19945c6fddcab3d3
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2025-08-20 11:40:21 -0700

    Added css styling to weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
-import Weather from './Weather';
+import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <h3>React Weather app</h3>
+        <Weather />
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/madison-robles-40155b235/"
+              target="blank"
+              rel="noreferrer"
+            >
+              Madison Robles
+            </a>
+            . It is open-sourced on{" "}
+            <a
+              href="https://github.com/mrsmrobles"
+              target="blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>{" "}
+            and hosted on{" "}
+            <a href="app.netlify.com" target="blank" rel="noreferrer">
+              Netlify
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,57 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div>
-      <h3>React Weather app</h3>
+    <div className="Weather">
       <form>
-        <input type="search" placeholder="Enter a city..." />
-        <button type="submit">Search</button>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary"
+              w-100
+            />
+          </div>
+        </div>
       </form>
       <h1>San Diego</h1>
-      <p>Wednesday 08:23am, sunny</p>
-      <p>Humidity: 60%, Wind: 5mph</p>
+      <ul>
+        <li>Wednesday 08:23am</li>
+        <li>Mostly Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="partly cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">73</span>
+              <span className="unit">°F</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 73%</li>
+            <li>Wind: 3 mph</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
